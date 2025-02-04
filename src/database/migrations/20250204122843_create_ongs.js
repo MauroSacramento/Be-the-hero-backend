5f1c8386d7c500231dce77ade079469cbc0c0d47
Maurosacramento <maurosacramento35@gmail.com>
2025-02-04 17:11:59 +0000

    Up to date

--- src/database/migrations/20250204122843_create_ongs.js
+++ src/database/migrations/20250204122843_create_ongs.js
@@ -7,7 +7,6 @@ exports.up = function(knex) {
         table.string('id').primary();
         table.string('name').notNullable();
         table.string('email').notNullable();
-        table.string('name').notNullable();
         table.string('whatsapp').notNullable();
         table.string('city').notNullable();
         table.string('uf', 2).notNullable();
