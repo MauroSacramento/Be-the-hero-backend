22a4db157a66934167d81f5af874f96ea6e1507b
Maurosacramento <maurosacramento35@gmail.com>
2025-02-05 12:50:45 +0000

    Backend finalizado

--- src/controllers/IncidentController.js
+++ src/controllers/IncidentController.js
@@ -3,8 +3,17 @@ const connection = require('../database/connection')
 module.exports = {
 
     async index(request, response) {
-        const incidents = await connection('incidents').select('*');
+        const {page = 1} = request.query;
 
+        const [count] = await connection('incidents').count() // Pra guardar o número completo dos casos
+
+        const incidents = await connection('incidents')
+        .join('ongs', 'ong_id', '=', 'incidents.ong_id')
+        .limit(5)
+        .offset((page -1) * 5)
+        .select(['incidents.*', 'ongs.name', 'ongs.email', 'ongs.whatsapp', 'ongs.city', 'ongs.uf']);
+
+        response.header('X-Total-Count', count['count('*')']); 
         response.json(incidents);
     },
 
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,17 @@
 const express = require('express')
 const ongController = require('./controllers/OngController');
 const incidentController = require('./controllers/IncidentController');
+const profileController = require('./controllers/ProfileController');
+const sessionController = require('./controllers/SessionController');
 
 const routes = express.Router();
 
 routes.get('/ongs', ongController.index);
 routes.post('/ongs', ongController.create);
+
+routes.post('/session', sessionController.create);
+
+routes.get('/profile', profileController.index);
 
 routes.post('/incidents', incidentController.create);
 routes.get('/incidents', incidentController.index);
